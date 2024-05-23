fb9ed46b04c3d6d159f7db11c071c66f5056e97a
Ryan Enns <ryanennns@gmail.com>
2024-05-23 16:37:12 -0400

    more work

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -16,6 +16,21 @@ const props = defineProps({
 onMounted(() => {
     console.log(props.posts);
 });
+
+const versionsOfNothing = [
+    "nothing",
+    "nada",
+    "rien",
+    "nichts",
+    "niente",
+    "ничего",
+    "niets",
+    "ingenting",
+    "nada",
+    "아무것도",
+]
+const noPosts = versionsOfNothing[Math.floor(Math.random()*versionsOfNothing.length)]
+
 </script>
 
 <template>
@@ -23,6 +38,10 @@ onMounted(() => {
         <div class="header">
             <h1 id="header">yakClone.</h1>
             <button id="createPost" @click=createPost><i class="fa-solid fa-pen"></i></button>
+        </div>
+        <div class="noPosts" v-if="props.posts.data.length === 0">
+            <p>{{noPosts}}.</p>
+            <p>Be the first one to make a post.</p>
         </div>
         <ul>
             <li v-for="post in props.posts.data">
@@ -33,7 +52,9 @@ onMounted(() => {
 </template>
 
 <style scoped>
+
 #wrapper {
+    height: 100vh;
     color: #4a4242;
     background-color: #FFFFF0;
     padding: 1rem 2rem;
@@ -63,5 +84,13 @@ onMounted(() => {
     border-radius: 5px;
     padding: 0.5rem;
     font-size: 1rem;
+}
+
+.noPosts {
+    color: #666;
+    font-size: 1.3rem;
+    font-style: italic;
+    text-align: center;
+    margin-top: 2rem;
 }
 </style>
